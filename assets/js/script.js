ffe5c9e181ddcda9636a7c242309aea5696a3913
Daniel Rodriguez <derodriguezc7@gmail.com>
2021-04-07 20:25:09 -0400

    Se agrega alert y favicon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
+
+    $("#github").click(function () {
+        alert("Presiona para ir a GitHub")
+    })
+    $("#twitter").click(function () {
+        alert("Presiona para ir a twitter")
+    })
+    $("#linkedin").click(function () {
+        alert("Presiona para ir a linkedin")
+    })
+    $("#facebook").click(function () {
+        alert("Presiona para ir a facebook")
+    })
 })
